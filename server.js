5b6b0df5a2a9cc524aed91ca2fb2d8ae12bbd503
souhailsellami96 <souhailsellami96@gmail.com>
2022-03-03 21:59:34 +0100

    root auth

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth",require("./routes/auth"));
+
 const PORT = process.env.PORT || 3000 ;
 app.listen(PORT,() => console.log('Server'));
